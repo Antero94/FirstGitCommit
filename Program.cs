f2dba0a46dcc3e75e5db12d4602c1fadf1bbfb60
Antero94 <haugland.jobb@gmail.com>
2023-01-21 09:52:40 +0100

    Jeg begynner å lære Git og Github!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Teste fil for Git commit.
+
+using System.ComponentModel;
+
+Console.WriteLine("Hello Git!");
+
+static void test()
+{
+    Console.WriteLine("\nThis function prints the multiplication table. Pick a number:");
+    int tall = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"\nPrinting multiplication table for {tall}:");
+    Console.WriteLine("");
+    for (int i = 1; i < 11; i++)
+        Console.WriteLine(tall * i);
+}
+
+test();
